b66f8cdf912223d4f56cd157b3804979af56a044
YEBBLEE <leeyeb9458@gmail.com>
2022-06-02 20:13:41 +0900

    qr코드 이미지 다운받기 구현

--- src/component/qr/qr.jsx
+++ src/component/qr/qr.jsx
@@ -9,6 +9,14 @@ function Qr() {
     QRCode.toCanvas(qrCanvas.current,'https://plicar.io/')
   },[]);
 
+  function handleQrBtn() {
+    const img = qrCanvas.current.toDataURL();
+    const link = document.createElement("a");
+    link.href = img;
+    link.download = 'QRCode';
+    link.click();
+  }
+
   return (
     <section className='qr'>
       <h1 className="qr-txt">스마트폰을 통해 스캔 해주세요.</h1>
@@ -17,7 +25,7 @@ function Qr() {
       </div>
       <p className="qr-info">스마트폰 기본 카메라 앱에서</p>
       <p className="qr-info">아래의 QR을 비춘 후 뜨는 링크를 눌러주세요.</p>
-      <button className='qr-btn'>QR코드 이미지 다운받기</button>
+      <button className='qr-btn' onClick={handleQrBtn}>QR코드 이미지 다운받기</button>
       <p className="qr-info">*AR기능은 현재 아래의 기기에서만 정상 작동 합니다.</p>
       <div className="info-box">
         <p className="qr-info">iPhone: iOS 12이상</p>
