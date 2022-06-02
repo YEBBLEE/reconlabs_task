381ec6c67f7ec4b02313617adb63df284190e3ed
YEBBLEE <leeyeb9458@gmail.com>
2022-06-02 18:03:30 +0900

    footer 추가

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../footer/footer.css';
+
+function Footer() {
+  return (
+    <section className="footer">
+      <p>Copyright 2022. RECON Labs Inc. All rights reserved</p>
+    </section>
+  );
+}
+
+export default Footer;
