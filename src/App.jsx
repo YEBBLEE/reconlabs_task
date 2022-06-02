ff6f5318c29b72acb3681a78694c2f814897fbe1
YEBBLEE <leeyeb9458@gmail.com>
2022-06-02 19:20:12 +0900

    app컴포넌트에 modal추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import Header from './component/header/header'
 import Viewer from './component/viewer/viewer';
 import Footer from './component/footer/footer';
 import Qr from './component/qr/qr';
+import Modal from './component/modal/modal';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModal = () => {
+    setModalOpen(!modalOpen);
+  }
+
   return (
     <>
       <Header/>
-      <Viewer/>
+      <Viewer onModalClick={handleModal}/>
       <Qr/>
       <Footer/>
+      {modalOpen && <Modal onModalClick={handleModal}/>}
     </>
     
   );
